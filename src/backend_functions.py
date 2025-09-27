132b9d233720b0978538fc83321a6e6e4895ccdd
Sai <lyricalmusicvideos2607@gmail.com>
2025-09-27 18:25:42 +0530

    Added translation backend, gave a logo prototype and added system tray functionality

--- src/backend_functions.py
+++ src/backend_functions.py
@@ -5,8 +5,24 @@ import keyboard
 import pyautogui
 import time
 import pyperclip
+from libretranslate.main import main as lt_main
+import threading
+import webview
+
+
+Translation_Windows_List = []
+
+
+with open("settings.json","r") as settings_json_file:
+        settings_content = json.load(settings_json_file)
+        TRANSLATE_SHORTCUT_KEY = settings_content["Short_Cut_Keys"]["Translate"]
+        print(TRANSLATE_SHORTCUT_KEY)
+        settings_json_file.close()
+
 class BACKEND_FUNCTIONS:
     def __init__(self):
+        translation_server_thread = threading.Thread(target=self.run_translation_service, daemon=True)
+        translation_server_thread.start()
         with open("settings.json","r") as settings_json_file:
             settings_content = json.load(settings_json_file)
             self.USER_NAME = settings_content["Name"]
@@ -14,24 +30,49 @@ class BACKEND_FUNCTIONS:
             self.GOOGLE_TRANSLATE_KEY = settings_content["Api_Keys"]["GOOGLE_TRANSLATE"]
             self.TRANSLATE_SHORTCUT_KEY = settings_content["Short_Cut_Keys"]["Translate"]
             
-    
-    def Button_Click(self,string):
-        print("Button Clicked!")
+
+    def run_translation_service(self):
+        lt_main()
         
 
 
-def grab_text_selection():
-    old_content = pyperclip.paste()
-    pyautogui.hotkey("ctrl","c")
-    time.sleep(0.05)
-    return pyperclip.paste(), old_content
 
-def on_hotkey():
-    text_to_translate, old_content = grab_text_selection()
-    pyperclip.copy(old_content)
-    if text_to_translate.strip():        
-        print("Captured:", text_to_translate)
+
+
+def grab_text_selection():
+    try:
+        old_content = pyperclip.paste()
+        pyautogui.hotkey("ctrl","c")
+        time.sleep(0.05)
+        new_content = pyperclip.paste()
+        if new_content == old_content:
+            print("NO NEW CONTENT WAS SELECTED, NOT CONTINUING")
+        else:
+            return new_content, old_content
+    except:
+        pass
+
+def on_translate_hotkey():
+    try:
+        text_to_translate, old_content = grab_text_selection()
+        #pyperclip.copy(old_content)
+        if text_to_translate.strip():        
+            print("Captured:", text_to_translate)
+
+            TRANSLATE_REQUEST_INFO = {
+                "q" : text_to_translate,
+                "source": "auto",
+                "target": "en",
+                "format": "text",
+                "alternatives": 3
+            }
+            translation_response = requests.post(url="http://127.0.0.1:5000/translate",data=TRANSLATE_REQUEST_INFO)
+            print(translation_response.json())
+    except:
+        pass
+
+
 
 def translate_hotkey_listener():
-    keyboard.add_hotkey('ctrl+shift+t', on_hotkey)  # custom shortcut
+    keyboard.add_hotkey(TRANSLATE_SHORTCUT_KEY, on_translate_hotkey)
     keyboard.wait()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,34 @@
 import os
 import json
-import time
 import webview
 import util_func
 import pyautogui
-import requests
 import backend_functions
-import keyboard
 import threading
 import pyautogui
-import pyperclip
+from argostranslate import package as argos_package
+import argostranslate.translate
+import pathlib
+import pystray
+from PIL import Image
+import sys
+
+
+def create_image():
+    image = Image.open("images\logo_1.png")
+    return image
+
+def on_quit(icon, item):
+    icon.stop()
+    MainWindow.destroy()
+    sys.exit()
+
+def on_window_close(window):
+    print("yess")
+    window.hide()
+    return False
+
+
 
 DEFAULT_SETTINGS_JSON = """{
   "Name": "USER_NAME",
@@ -18,7 +37,7 @@ DEFAULT_SETTINGS_JSON = """{
     "GOOGLE_TRANSLATE": ""
   },
   "Short_Cut_Keys": {
-    "Translate": "ctrl+shift+t"
+    "Translate": "ctrl+shift+y"
   }
 }
 """
@@ -28,7 +47,6 @@ screen_width, screen_height = pyautogui.size()
 
 
 Appwidth, Appheight = util_func.scaled_for_screen(1200,750,2560,1440,screen_width,screen_height)
-print(Appwidth,Appheight)
 
 print(f" CURRENT DIRECTORY: {CURRENT_DIRECTORY}")
 
@@ -36,8 +54,6 @@ if os.path.isfile(f"{CURRENT_DIRECTORY}/settings.json"):
     print("NOT FIRST STARTUP: READING SETTINGS")
     with open("settings.json","r") as settings_json_file:
         settings_content = json.load(settings_json_file)
-        print(settings_content["Name"])
-        print(settings_content["Api_Keys"]["OPEN_AI"])
 
         USER_NAME = settings_content["Name"]
         OPEN_AI_KEY = settings_content["Api_Keys"]["OPEN_AI"]
@@ -51,19 +67,43 @@ else:
     print("CREATING SETTINGS.JSON FILE")
     with open("settings.json","w") as settings_json_file:
         settings_json_file.write(DEFAULT_SETTINGS_JSON)
+    
+    models_folder = pathlib.Path("translate_models")
+
+    # Install all packages in the folder
+    for model_file in models_folder.glob("*.argosmodel"):
+      print(f"Installing {model_file.name}...")
+      argos_package.install_from_path(model_file)
+
+
+    argostranslate.translate.load_installed_languages()
+    
         
 
-window = webview.create_window(
+MainWindow = webview.create_window(
     "Amethyst",
     "frontend/main.html", 
     width=Appwidth,
     height=Appheight,
     resizable=False,
-    js_api=backend_functions.BACKEND_FUNCTIONS()
+    js_api=backend_functions.BACKEND_FUNCTIONS(),
+    
 )
 
+MainWindow.events.closing += on_window_close
+
+def setup_tray():
+    icon = pystray.Icon("Amethyst", create_image(), "Amethyst")
+    icon.menu = pystray.Menu(
+      pystray.MenuItem("Show App", lambda: MainWindow.show()),
+      pystray.MenuItem("Quit", on_quit),
+    )
+    icon.run()
+
+threading.Thread(target=setup_tray, daemon=True).start()
 threading.Thread(target=backend_functions.translate_hotkey_listener, daemon=True).start()
 
-webview.start(gui='edgechromium', func=None, debug=False, http_server=True)
+
+webview.start(gui='edgechromium', func=None, debug=False, http_server=True,ssl=True)
 
 
